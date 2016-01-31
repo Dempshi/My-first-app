4bfb161dca44f6b629644e6ef47ed9d1610bc084
Cory McFadden <cormcf34@hotmail.com>
2016-01-31 14:41:14 +0000

    Build show and validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,23 @@ class ArticlesController < ApplicationController
     @article = Article.new
     
   end
+
   
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirict_to article_show(@article)
+    
+    if @article.save
+      flash[:notice] = 'Congratulations your article has been successfully created'
+      redirect_to article_path(@article)
+      
+    else 
+      render 'new'
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
@@ -17,6 +28,5 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :description)
     
   end
-  
   
 end
